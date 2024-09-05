9a65968549ddce8fd272ac125c0fe8e2f8140ff1
Kamaleshsarkar89189 <skamalesh865@gmail.com>
2024-09-05 13:14:37 +0530

    added new file

---
+++ Module_1/CelciousToFaharenheit.py
@@ -0,0 +1,19 @@
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+print("Choose the conversion type:")
+print("1) Celsius to Fahrenheit")
+print("2) Fahrenheit to Celsius")
+choice = input("Enter your choice (1/2 ): ")
+if choice == '1':
+    celsius = float(input("Enter temperature in Celsius: "))
+    fahrenheit = celsius_to_fahrenheit(celsius)
+
+    print(celsius,"C is equal to ",fahrenheit,"F")
+elif choice == '2':
+    fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+    celsius = fahrenheit_to_celsius(fahrenheit)
+    print(fahrenheit,"F is equal to ",celsius,"C")
+else:
+    print("Invalid choice! Please choose either '1' or '2'.")
